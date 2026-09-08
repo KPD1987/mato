f9a9f4cc10890e63bab2829a03d65b08750579f5
KPD <kpd__87@hotmail.com>
2026-09-08 11:03:08 +0300

    Update game.js

--- game.js
+++ game.js
@@ -4,7 +4,6 @@ const ctx = canvas.getContext("2d");
 
 // HTML-elementtien haku
 const startButton = document.getElementById("startButton"); // Aloita peli -nappi
-const enterButton = document.getElementById("enterButton"); // Enter-nappi, jolla voi myös aloittaa pelin
 const restartButton = document.getElementById("restartButton"); // Aloita uusi peli -nappi pelin päättyessä
 const escapeButton = document.getElementById("escapeButton"); // Lopeta peli -nappi
 const menu = document.getElementById("menu"); // Päävalikko
@@ -21,17 +20,25 @@ const height = canvas.height;
 const cellSize = 15;
 
 // Pelin muuttujat
-let snake;
-let direction; // Madon koordinaatit (x, y)
-let fruit; // syötävän hedelmän koordinaatit (x, y)
-let score; // pisteet
-let fruitCount; // hedelmien lukumäärä, jotta voidaan tarkistaa, milloin mato kasvaa
+let snake =  {
+    xPos: 15,
+    yPos: 15,
+    fruits: 0,
+    length: 3,
+    speed: 250,
+    direction: null
+}; //käärmeeseen tallennettu data
+let fruit = {
+    xPos: 20,
+    yPos: 20
+}; // syötävän hedelmän koordinaatit (x, y)
+let score = 0; // pisteet
 let gameLoopInterval; // Pelin loopin interval
 let gameRunning = false; // Tarkistaa, onko peli ajossa
-let _currentSpeed = 250; // Pelin nopeus (millisekunteina, kuinka usein pelin logiikka päivittyy)
+let boardSize = 16;
+let difficulty = 1; // 0 = easy, 1 = normal, 2 = hard, 3 = impossible
 
 // Kontrollit
-document.addEventListener("enter", (e) => {startGame()});
 document.addEventListener("Escape", (e) => {
     if (gameRunning) {
         endGame("Game Over");
@@ -39,29 +46,57 @@ document.addEventListener("Escape", (e) => {
 });
 document.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
-        startGame();
+        if (gameRunning) return;
+        startGame(boardSize, difficulty);
         return;
-    }
+        }
+});
+document.addEventListener("keydown", (e) => {
     if (!gameRunning) return;
-
     else if (e.key === "Escape") endGame("Game Over");
-
     switch (e.key) {
         case "ArrowUp":
-            if (direction.y === 0) direction = { x: 0, y: -1 };
+            snake.direction = "up"
+            console.log("up arrow pressed")
             break;
         case "ArrowDown":
-            if (direction.y === 0) direction = { x: 0, y: 1 };
+            snake.direction = "down"
+            console.log("down arrow pressed")
             break;
         case "ArrowLeft":
-            if (direction.x === 0) direction = { x: -1, y: 0 };
+            snake.direction = "left"
+            console.log("left arrow pressed")
             break;
         case "ArrowRight":
-            if (direction.x === 0) direction = { x: 1, y: 0 };
+            snake.direction = "right"
+            console.log("right arrow pressed")
             break;
     }
 });
 
+function startGame(size, diff) {
+    console.log("game start triggered");
+    gameRunning = true;
+    setInterval(movement, snake.speed);
+};
+
+function movement() {
+    switch (snake.direction) {
+        case "up":
+            snake.yPos += 1;
+            break;
+        case "down":
+            snake.yPos -= 1;
+            break;
+        case "right":
+            snake.xPos += 1;
+            break;
+        case "left":
+            snake.xPos -= 1;
+            break;
+    }
+    console.log(snake.xPos, snake.yPos, snake.direction)
+};
+
 startButton.addEventListener("click", startGame);
 restartButton.addEventListener("click", startGame);
-startButton.addEventListener("enter", startGame);
